a70359f8b6f173552c411890b60ca3d7ffd25d41
lynz <2821260061@qq.com>
2024-08-12 19:38:03 +0800

    新建项目

---
+++ src/main/java/com/lynz/logisticsmanagementsystem/LogisticsManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.lynz.logisticsmanagementsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LogisticsManagementSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LogisticsManagementSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/lynz/logisticsmanagementsystem/LogisticsManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lynz.logisticsmanagementsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LogisticsManagementSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
