486a04daf45b0e9af2299c78f9522570ea362132
lynz <2821260061@qq.com>
2024-08-14 00:01:43 +0800

    修改部分bug

--- src/main/java/com/lynz/logisticsmanagementsystem/controller/UserController.java
+++ src/main/java/com/lynz/logisticsmanagementsystem/controller/UserController.java
@@ -4,12 +4,14 @@ import com.lynz.logisticsmanagementsystem.pojo.User;
 import com.lynz.logisticsmanagementsystem.service.serviceImpl.UserServiceImpl;
 import com.lynz.logisticsmanagementsystem.util.Result;
 import com.lynz.logisticsmanagementsystem.util.ResultUtil;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @SuppressWarnings({"all"})
 @RestController
 @RequestMapping("/user")
@@ -18,9 +20,9 @@ public class UserController {
     UserServiceImpl userServiceImpl;
 
     @RequestMapping("/login")
-    public Result login(@RequestParam String username, @RequestParam String password) {
+    public Result login(@RequestParam("username") String username, @RequestParam("password") String password) {
         String msg = userServiceImpl.loginService(username, password);
-        if (("SUCCESS").equals(msg)) {
+        if (("success").equals(msg)) {
             return ResultUtil.success("登录成功");
         }else {
             return ResultUtil.error(msg);
@@ -29,8 +31,10 @@ public class UserController {
 
     @RequestMapping("/register")
     public Result register(@RequestBody User user) {
+        log.info(user.toString());
         String msg = userServiceImpl.registerService(user);
-        if (("SUCCESS").equals(msg)) {
+        log.info(msg);
+        if (("success").equals(msg)) {
             return ResultUtil.success("注册成功");
         }else {
             return ResultUtil.error(msg);
